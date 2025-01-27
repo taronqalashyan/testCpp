92e60532dddb74d3febfc12feaa47292e231ead8
Taron Kalashyan <taron_k@MC-0613.local>
2025-01-27 12:15:13 +0400

    - Foo

---
+++ less1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+int main() {
+    int a = 0;
+    int b = 0;
+    char c = ''; // Initialize with a space or valid default value
+
+    std::cout << "Enter two numbers and an operator (+, -, *, /):\n";
+    std::cin >> a >> b >> c;
+
+    switch (c) {
+        case '-':
+            std::cout << "a - b = " << a - b << "\n";
+            break;
+        case '+':
+            std::cout << "a + b = " << a + b << "\n";
+            break;
+        case '*':
+            std::cout << "a * b = " << a * b << "\n";
+            break;
+        case '/':
+            if (b != 0)
+                std::cout << "a / b = " << a / b << "\n";
+            else
+                std::cout << "Error: Division by zero\n";
+            break;
+        default:
+            std::cout << "Invalid operator. Please use +, -, *, or /.\n";
+            break;
+    }
+
+    return 0;
+}
+
